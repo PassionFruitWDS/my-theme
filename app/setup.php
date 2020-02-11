85c72ef0d0e27ae30dedae4fa65b5e7dbdf18d60
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-02-11 15:17:30 -0600

    Define color palette and defaults

--- app/setup.php
+++ app/setup.php
@@ -112,13 +112,61 @@ add_action('after_setup_theme', function () {
 	 * Enable theme color palette support
 	 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-color-palettes
 	 */
-	add_theme_support('editor-color-palette', [
-		[
-			'name'  => __('Primary', 'sage'),
-			'slug'  => 'primary',
-			'color' => '#525ddc',
-		]
-	]);
+	add_theme_support(
+		'editor-color-palette',
+		array(
+			array(
+				'name'  => __('Primary', 'sage'),
+				'slug'  => 'primary',
+				'color' => '#EC7A00',
+			),
+			array(
+				'name' => __('Secondary', 'sage'),
+				'slug' => 'secondary',
+				'color' => '#0072EC'
+			),
+			array(
+				'name' => __('Dark Primary', 'sage'),
+				'slug' => 'dark-primary',
+				'color' => '#DC5100'
+			),
+			array(
+				'name' => __('Light Primary', 'sage'),
+				'slug' => 'light-primary',
+				'color' => '#F8B44D'
+			),
+			array(
+				'name' => __('Dark Secondary', 'sage'),
+				'slug' => 'dark-secondary',
+				'color' => '#173FBA'
+			),
+			array(
+				'name' => __('Light Secondary', 'sage'),
+				'slug' => 'light-secondary',
+				'color' => '#56B4FF'
+			),
+			array(
+				'name' => __('Primary Accent', 'sage'),
+				'slug' => 'primary-accent',
+				'color' => '#EC0400'
+			),
+			array(
+				'name' => __('Primary Accent Alternate', 'sage'),
+				'slug' => 'primary-accent-alternate',
+				'color' => '#E8EC00'
+			),
+			array(
+				'name' => __('Secondary Accent', 'sage'),
+				'slug' => 'secondary-accent',
+				'color' => '#72EC00'
+			),
+			array(
+				'name' => __('Secondary Accent Alternate', 'sage'),
+				'slug' => 'secondary-accent-alternate',
+				'color' => '#00EC7A'
+			)
+		)
+	);
 }, 20);
 
 /**
