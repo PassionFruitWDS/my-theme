9f56120b56a50582003917bb8cc176ae2dcf7406
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-02-16 18:47:40 -0600

    Style primary nav menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,10 @@
 <header class="banner">
-	<div class="container">
+	<nav class="nav-primary has-primary-background-color">
 		<a class="brand" href="{{ home_url('/') }}">
 			{{ $siteName }}
 		</a>
-
-		<nav class="nav-primary">
-			@if (has_nav_menu('primary_navigation'))
-				{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-			@endif
-		</nav>
-	</div>
+		@if (has_nav_menu('primary_navigation'))
+			{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+		@endif
+	</nav>
 </header>
