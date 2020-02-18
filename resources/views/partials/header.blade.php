c2992b289342f6d06c52c1806f1a97dc1c97c191
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-02-18 17:17:45 -0600

    Add hamburger to primary-nav html

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,18 @@
 		</a>
 		@if (has_nav_menu('primary_navigation'))
 			{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+			<label class="hamburger-label">
+				<button class="hamburger hamburger--emphatic"
+					type="button"
+					aria-label="Menu"
+					aria-controls="navigation"
+				>
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</button>
+				MENU
+			</label>
 		@endif
 	</nav>
 </header>
