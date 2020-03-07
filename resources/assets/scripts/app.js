6f7f5598af20da5a9ed03a199e4dc75e069c9e94
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-03-07 13:44:54 -0600

    Script contact form reactivity

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -23,4 +23,32 @@ $(document).ready(() => {
 			blockElement.addClass(cssClass);
 		}
 	});
+
+	// toggle expansion of contact form
+	const mainField = $('.contact-form__main-wrapper input');
+	mainField.on('input', event => {
+		const targetElement = $(event.target);
+		const formId = targetElement.attr('form');
+		const form = $(`#${formId}`);
+		const cssClass = 'contact-form--is-active';
+		if (!form.hasClass(cssClass)) {
+			const hero = form.parent('.hero');
+			const gridHeights = hero.css('grid-template-rows').split(' ');
+			const shimHeight = gridHeights[2];
+			hero.append(`<style type="text/css">.hero::before {grid-area: shim; content: ""; min-height: ${shimHeight};</style>`);
+			form.css('margin', `${form.css('margin-top')} 0`);
+			const template = $(`#${formId}__remainder`);
+			form.append(template.html());
+			form.find('.button--submit').removeAttr('disabled');
+			form.addClass(cssClass);
+		} else {
+			if (form.find('input:not(.button)').toArray().every(element => !$(element).val())) {
+				form.children('.contact-form__field').remove();
+				form.removeClass(cssClass);
+				form.css('margin', `auto 0`);
+				form.find('.button--submit').attr('disabled', true);
+				form.parent('.hero').children('style').remove();
+			}
+		}
+	});
 });
