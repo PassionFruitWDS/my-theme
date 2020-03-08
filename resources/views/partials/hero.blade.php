1630dd4aaee374c6dc01f15746a537c38b5d2731
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-03-07 18:32:55 -0600

    Add hero partial template

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,20 @@
+<section class="hero">
+	<div class="hero__background"
+		style="background-image: url({{ get_template_directory_uri() . '/dist/images/hero.jpg' }})">
+	</div>
+	<div class="hero__brand-grid">
+		<div class="hero__brand-surface"></div>
+		<img class="hero__brand-image"
+			src="{{ get_template_directory_uri() . '/dist/images/pfwds_logo.png' }}"
+			alt="PassionFruit Web Development Studio"/>
+	</div>
+	<div class="hero__feature-grid">
+		<div class="hero__feature-surface"></div>
+		<div class="hero__feature">
+			<h2>Your business, your passion...</h2>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			</p>
+		</div>
+	</div>
+</section>
