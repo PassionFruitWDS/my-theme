88db0cc0ab09ea148b542562d719eff7c68d7d4a
Augustus Buonviri <passionfruitwebdev@gmail.com>
2020-03-28 09:30:58 -0500

    Script tri-content reactivity

--- app/setup.php
+++ app/setup.php
@@ -19,6 +19,8 @@ use function Roots\asset;
 add_action('wp_enqueue_scripts', function () {
 	wp_enqueue_script('sage/vendor.js', asset('scripts/vendor.js')->uri(), ['jquery'], null, true);
 	wp_enqueue_script('sage/app.js', asset('scripts/app.js')->uri(), ['sage/vendor.js', 'jquery'], null, true);
+	$translation_array = array( 'themeUrl' => get_template_directory_uri() );
+	wp_localize_script('sage/app.js', 'injectedData', $translation_array);
 
 	wp_add_inline_script('sage/vendor.js', asset('scripts/manifest.js')->contents(), 'before');
 
--- resources/views/partials/tri-content.blade.php
+++ resources/views/partials/tri-content.blade.php
@@ -2,7 +2,7 @@
 	$tri_content_id = "tri-content-1";
 ?>
 
-<section class="tri-content">
+<section id="{{$tri_content_id}}" class="tri-content">
 	<span class="tri-content__ctr">
 		<button id="{{$tri_content_id}}__back" class="tri-content__button"><</button>
 		<span id="{{$tri_content_id}}__prev" class="tri-content__heading">Design</span>
@@ -11,16 +11,16 @@
 		<button id="{{$tri_content_id}}__frwd" class="tri-content__button">></button>
 	</span>
 	<div class="tri-content__content-area">
-		<div id="{{$tri_content_id}}__left"
+		<div id="{{$tri_content_id}}__content"
 			class="tri-content__content tri-content__content--left">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</div>
-		<div id="{{$tri_content_id}}__right"
-			class="tri-content__content tri-content__content--right">
-			<img class="pfwds-towers__background" src="{{ get_template_directory_uri() . '/dist/images/passionfruit_towers.png' }}"/>
-			<img class="pfwds-towers__icon pfwds-towers__icon--one" src="{{ get_template_directory_uri() . '/dist/images/at.png' }}"/>
-			<img class="pfwds-towers__icon pfwds-towers__icon--two" src="{{ get_template_directory_uri() . '/dist/images/search.png' }}"/>
-			<img class="pfwds-towers__icon pfwds-towers__icon--three" src="{{ get_template_directory_uri() . '/dist/images/share.png' }}"/>
+		<div id="{{$tri_content_id}}__image"
+			class="tri-content__content tri-content__content--right pfwds-towers">
+			<img id="{{$tri_content_id}}__background" class="pfwds-towers__background" src="{{ get_template_directory_uri() . '/dist/images/passionfruit_towers.png' }}"/>
+			<img id="{{$tri_content_id}}__icon--one" class="pfwds-towers__icon pfwds-towers__icon--one" src="{{ get_template_directory_uri() . '/dist/images/at.png' }}"/>
+			<img id="{{$tri_content_id}}__icon--two" class="pfwds-towers__icon pfwds-towers__icon--two" src="{{ get_template_directory_uri() . '/dist/images/search.png' }}"/>
+			<img id="{{$tri_content_id}}__icon--three" class="pfwds-towers__icon pfwds-towers__icon--three" src="{{ get_template_directory_uri() . '/dist/images/share.png' }}"/>
 		</div>
 	</div>
 </section>
